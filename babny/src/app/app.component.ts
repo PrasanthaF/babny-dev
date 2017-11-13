4ce671c67eb39da12d09a8a046aa48f8095822e9
Prasantha Fernando <fprasantha@gmail.com>
2017-11-13 14:47:07 -0500

    Initial Angular Cli setup

---
+++ babny/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'babny-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'babny';
+}
